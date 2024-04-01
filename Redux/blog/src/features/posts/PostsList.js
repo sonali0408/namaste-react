7f8550d92335da4f34abf98f95f97a83615b0bc0
sonali0408 <sonalipt04@gmail.com>
2024-04-02 00:05:21 +0530

    Redux project

---
+++ Redux/blog/src/features/posts/PostsList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectAllPosts, getPostsError, getPostsStatus } from "./postsSlice";
+import PostsExcerpt from "./PostsExcerpt";
+
+const PostsList = () => {
+  const postsList = useSelector(selectAllPosts);
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
+  console.log("Hello", postsList);
+
+  let content;
+  if (postStatus === "loading") {
+    content = <p>"Loading..."</p>;
+  } else if (postStatus === "succeeded") {
+    const orderedPosts = postsList
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => (
+      <PostsExcerpt key={post.id} post={post} />
+    ));
+  } else if (postStatus === "failed") {
+    content = <p>{error}</p>;
+  }
+  return (
+    <section>
+      {content}
+    </section>
+  );
+};
+
+export default PostsList;
