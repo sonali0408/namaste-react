771565b21d296d0c5febc5b3adb51183ff4ab426
sonali0408 <sonalipt04@gmail.com>
2024-04-02 01:41:59 +0530

    fix

--- Redux/blog/src/features/posts/PostsExcerpt.js
+++ Redux/blog/src/features/posts/PostsExcerpt.js
@@ -2,13 +2,13 @@ import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 import { Link } from "react-router-dom";
+import React from "react";
 
 import { useSelector } from "react-redux";
 import { selectPostById } from "./postsSlice";
 
 const PostsExcerpt = ({ postId }) => {
   const post = useSelector((state) => selectPostById(state, postId));
-
   return (
     <article>
       <h2>{post.title}</h2>
